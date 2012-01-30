19d92005697e800efa9f04c0c730aefac6072377
Marko Black <marko.blck@gmail.com>
2012-01-31 00:32:27 +0200

    updated lab02 src

--- lab02/prog/main.c
+++ lab02/prog/main.c
@@ -1,47 +1,38 @@
 #include <reg52.h>
 #include "utils.h"
 
+#define INDICATORS_NUM 8
+
 sfr outIndSelectorPort = 0x90;
 sfr outSymbolPort = 0xA0;
 
-unsigned char secsCount = 0;
-unsigned char minsCount = 0;
-unsigned char hoursCount = 0;
-
-code unsigned char symbolsMap[] = { 
-	0x03, 0xCF, 0x91, 0x87, 0x4D, 0x25, 0x21, 0x8F, 0x01, 0x05 };
-unsigned char symbols[4] = {0xFF};
-
-void UpdateTime()
-{
-	switch (CheckTimer()) {
-	case 2:
-		StartTimer(1000);
-		break;
-	case 1:
-		StartTimer(1000);
-		secsCount++;
-		symbols[3] = symbolsMap[secsCount%10];
-		break;
-	default:
-		break;
-	} 
-}
-
-void UpdateIndOut(void)
-{
-	static unsigned char count = 0;
-	outIndSelectorPort = ~(0x01 << (count%8));
-	outSymbolPort = 0x91;
-	count++;
-}
+unsigned char symsMap[] = {
+	0xfc, 0x60, 0xda, 0xf2, 0x66, 0xb6, 0xbe, 0xe0, 0xfe, 0xf6 };
 
 void main(void)
 {
 	InitUtils();	
 
 	while(1) {
-		UpdateTime();
-		UpdateIndOut();
+
+		/* update 7-segment display information */
+		for (i = 0; i < INDICATORS_NUM; i++) {
+			/* activate next indicator */
+			indicatorsPort = ~(0x01 << i);
+			/* update active indicator symbol */
+			segsPort = outInfo[i];
+		}
+
+		/* scan keyboard for changes */
+		for (i = 0; i < SCAN_TRIES; i++) {
+			for (j = 0; j < KBD_ROWS_NUM; j++) {
+				/* select appropriate keys row */
+				keysPort |= 0x70;
+				keys = keysPort&0x70;
+				/* scan keys column */
+				keysPort |= 0x0f;
+				keys = keysPort&0x0f;
+			}
+		}
 	}
 }
