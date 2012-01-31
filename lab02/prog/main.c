49ef1a5f8ed0c6dbe6a86eca7b0e57a9565b10e8
unknown <user@.(none)>
2012-01-31 12:56:26 +0200

    updated lab02 sample code

--- lab02/prog/main.c
+++ lab02/prog/main.c
@@ -1,38 +1,95 @@
 #include <reg52.h>
-#include "utils.h"
+#include "types.h"
 
-#define INDICATORS_NUM 8
+#define INDICATORS_NUM	4
+#define KBD_ROWS_NUM	4
+#define KBD_COLS_NUM	3
+#define KBD_SCAN_TRIES	3
 
-sfr outIndSelectorPort = 0x90;
-sfr outSymbolPort = 0xA0;
+#define SIZEOF_ARRAY(arr) (sizeof(arr)/sizeof(arr[0]))
 
-unsigned char symsMap[] = {
+sfr indicatorsPort 	= 0x90;
+sfr segmentsPort 	= 0xA0;
+sfr keysPort 		= 0xB0;
+
+static uint8_t activeIndicator 	= 0;
+static uint8_t activeCount 		= 0;
+
+code uint8_t symsMap[] = {
 	0xfc, 0x60, 0xda, 0xf2, 0x66, 0xb6, 0xbe, 0xe0, 0xfe, 0xf6 };
+
+uint8_t outInfo[INDICATORS_NUM] = {0};
+
+uint8_t GetKeyPressed(void)
+{
+	uint8_t i, j, k, keys;
+	uint8_t key[KBD_COLS_NUM] = {0};
+
+	/* scan keyboard for changes */
+	/* scan keys column several times in order to avoid a jitter */
+	for (k = 0; k < KBD_SCAN_TRIES; k++) {
+		for (i = 0; i < KBD_ROWS_NUM; i++) {
+			uint8_t keyMask = 0x01;
+
+			keysPort = 0x7f&(0x10<<i);
+			keys = keysPort&0x0f;
+
+			for (j = 0; j < KBD_COLS_NUM; j++) {
+				key[j] += (keys&keyMask)>>j;
+
+				/* return first key code which is in high level 2 times */				
+				if (key[j] == 2) {
+					return j + i*KBD_COLS_NUM;
+				}
+
+				keyMask <<= 1;
+			}
+		}
+	}
+
+	return KBD_ROWS_NUM*KBD_COLS_NUM;
+}
+
+void UpdateDisplay(void)
+{
+	static uint8_t i = 0;
+
+	/* update 7-segment display information - update only one indicator in order to have
+	   it powered for loger time - ti increase it's light intensity */
+
+	/* activate next indicator */
+	indicatorsPort = ~(0x01 << i);
+	/* update active indicator symbol */
+	if (activeIndicator != i) {
+		segmentsPort = outInfo[i];
+	} else {
+		if (!activeCount) {
+			segmentsPort = outInfo[i];
+		} else {
+			segmentsPort = 0x00;
+		}
+		
+		activeCount++;	
+	}
+
+	i++;
+	i %= INDICATORS_NUM;
+}
 
 void main(void)
 {
-	InitUtils();	
-
 	while(1) {
+		uint8_t key = GetKeyPressed();
 
-		/* update 7-segment display information */
-		for (i = 0; i < INDICATORS_NUM; i++) {
-			/* activate next indicator */
-			indicatorsPort = ~(0x01 << i);
-			/* update active indicator symbol */
-			segsPort = outInfo[i];
+		if (key < SIZEOF_ARRAY(symsMap)) {
+			/* update output synbol for active indicator appropriate to pressed key */
+			outInfo[activeIndicator] = symsMap[key];
+		} else if (key == SIZEOF_ARRAY(symsMap)) {
+			/* shift active indicator right/left */
+			activeIndicator++;
+			activeIndicator %= INDICATORS_NUM;
 		}
 
-		/* scan keyboard for changes */
-		for (i = 0; i < SCAN_TRIES; i++) {
-			for (j = 0; j < KBD_ROWS_NUM; j++) {
-				/* select appropriate keys row */
-				keysPort |= 0x70;
-				keys = keysPort&0x70;
-				/* scan keys column */
-				keysPort |= 0x0f;
-				keys = keysPort&0x0f;
-			}
-		}
+		UpdateDisplay();		
 	}
 }
