5188989fae5c2e64b0fb8ed57f6945053f87349a
Marko Black <marko.blck@gmail.com>
2013-04-13 20:17:39 +0300

    added skiped header

---
+++ c51/Roborace/linescan.h
@@ -0,0 +1,10 @@
+#ifndef _LINE_SCAN_MODULE_H_
+#define _LINE_SCAN_MODULE_H_
+
+#include <reg52.h>
+#include "../lib/types.h"
+#include "config.h"
+
+uint8_t LINE_SCAN_GetState();
+
+#endif /*_LINE_SCAN_MODULE_H_*/
